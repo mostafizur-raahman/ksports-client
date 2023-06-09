0910dfa1cea4674cebfab4c7e4bfc7b7020af2f8
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-06-09 11:55:20 +0600

    initial project set up

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <div className="mx-20">
+            <RouterProvider router={router}></RouterProvider>
+        </div>
+    </React.StrictMode>
+);
