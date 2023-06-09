b95257eea71a3fc19a572dbf4e62b31c10cd2a40
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-06-09 11:59:26 +0600

    firebase set up

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwtsMKZjMwjBupXNOTi41sPVDrRBqS4tk",
+  authDomain: "k-sports-87416.firebaseapp.com",
+  projectId: "k-sports-87416",
+  storageBucket: "k-sports-87416.appspot.com",
+  messagingSenderId: "248150038162",
+  appId: "1:248150038162:web:e115d0b2d6e99e417df84b"
+};
+
+export const app = initializeApp(firebaseConfig);
